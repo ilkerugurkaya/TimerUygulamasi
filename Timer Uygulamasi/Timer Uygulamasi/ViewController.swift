ebefe05452ee1e07ec06a7b3763351ddd4485ce0
ilkerugurkaya <112544173+ilkerugurkaya@users.noreply.github.com>
2022-12-06 09:45:29 +0300

    Add files via upload

---
+++ Timer Uygulamasi/Timer Uygulamasi/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Timer Uygulamasi
+//
+//  Created by İlker Kaya on 4.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var sayici:Timer?
+    var sayac1: Int = 1
+    var sayac2:Int = 5
+    
+    
+    
+    @IBOutlet weak var ciktiLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        ciktiLabel.text = ""
+    }
+
+
+    @IBAction func basla(_ sender: Any) {
+        sayici = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(geriSay), userInfo: nil, repeats: true)
+    }
+    @objc func ileriSay(){
+        ciktiLabel.text = String(sayac1)
+        sayac1 += 1
+        
+        if sayac1 > 6 {
+            sayici?.invalidate()
+            ciktiLabel.text = "Süre bitti"
+            sayac1 = 1
+        }
+    }
+    @objc func geriSay(){
+        ciktiLabel.text = String(sayac2)
+        sayac2 -= 1
+        
+        if sayac2 < 0 {
+            sayici?.invalidate()
+            ciktiLabel.text = "Süre bitti"
+            sayac2 = 5
+        }
+    }
+    
+    
+}
+
